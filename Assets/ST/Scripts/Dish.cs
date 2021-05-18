df9c720dc955ecc7c6f3867bd4180a93a08acdbd
junjun1002 <ueno.junya@outlook.jp>
2021-05-18 20:22:41 +0900

    一年生かわいそうすぎワロタ また今度続きやろ

---
+++ Assets/ST/Scripts/Dish.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dish : MonoBehaviour
+{
+    /// <summary>ScoreManagerのオブジェクト</summary>
+    [SerializeField] ScoreManager m_ScoreManager;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Coin")
+        {
+
+        }
+    }
+}
---
+++ Assets/ST/Scripts/ScoreManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+
+    [SerializeField] int m_score = 1;
+
+    [SerializeField] Text m_scoreText;
+
+    int m_currentScore;
+
+    public void AddScore()
+    {
+        m_currentScore += m_score;
+        m_scoreText.text = m_currentScore.ToString();
+    }
+}
