62d9f51090b76ca07635dfeece750382b1f1bae2
junjun1002 <ueno.junya@outlook.jp>
2021-06-01 16:41:52 +0900

    やっぱりサイト通り作ったらやばいな わかりやすいように編集しますか まあまずは授業解説にとこまでやってから改造かな

---
+++ Assets/ST/Scripts/Breakout/Ball.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Breakout
+{
+    public class Ball : MonoBehaviour
+    {
+        [SerializeField] float m_speed = 5.0f;
+
+        [SerializeField] float m_maxSpeed = 10f;
+
+        Rigidbody m_rb;
+
+        private void Start()
+        {
+            m_rb = GetComponent<Rigidbody>();
+            m_rb.AddForce((transform.forward + transform.right) * m_speed, ForceMode.VelocityChange);
+        }
+
+        
+    }
+}
---
+++ Assets/ST/Scripts/Breakout/PlayerBar.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Breakout
+{
+    public class PlayerBar : MonoBehaviour
+    {
+        /// <summary>Playerが動くときに力を加えるか、速度を操作するか</summary>
+        [SerializeField] MoveType m_moveType = MoveType.AddForce;
+        /// <summary>動く時のスピード</summary>
+        [SerializeField] float m_moveSpeed = 1.0f;
+
+        Rigidbody m_rb;
+
+        void Start()
+        {
+            m_rb = GetComponent<Rigidbody>();
+        }
+
+        private void Update()
+        {
+            // 方向の入力を取得し、方向を求める　縦方向はVerticalで入力を取得できる
+            float h = Input.GetAxisRaw("Horizontal");
+            // dirはdirectionの略、意味は方向
+            // Vector3.rightはVector3(1, 0, 0)と同じ意味、入力に対して力を加えている
+            Vector3 dir = Vector3.right * h;
+
+            // 入力があれば、そちらの方向に動かす　
+            // ！＝イコールじゃないよの意味
+            // ifはもし～
+            if (dir != Vector3.zero)
+            {
+                // 自分が動く方向の向きを代入している
+                this.transform.right = dir;
+                // Enumに関しては個人的Debugをしやすくするために使ってるので割愛（個人で聞いてくれたら答えます）
+                if (m_moveType == MoveType.AddForce)
+                {
+                    // Spawnerの動きをAddForceで動かす場合
+                    // AddForceはどんどん力加えていく関数、この場合だと動く向きにスピードを加算している
+                    m_rb.AddForce(this.transform.right * m_moveSpeed);
+                }
+                else if (m_moveType == MoveType.Velocity)
+                {
+                    // Spawnerの動きをvelocityで動かす場合
+                    // velocityは入力されたスピードで一定に動く関数
+                    m_rb.velocity = this.transform.right * m_moveSpeed;
+                }
+            }
+        }
+    }
+}
+
